2f1ae7bc70d9090d8787ed09d8e599dddf1a8d65
andreas@bizreach.co.jp <andreas@bizreach.co.jp>
2017-02-16 08:10:17 +0900

    CDefはJavaのInterfaceで、codeOf()やlistAll()のような実装済みメッソッドを持ってない。 なので、Cdefを実装している具体的なEnum達を引数として渡してあげて、それに対してgetCode()やlistAll()を呼びたいですが、CDefにはそういったメソッドが宣言されてなくて、 コードがコンパイル通らない…

--- macros/src/main/scala/extractors/FieldValidators.scala
+++ macros/src/main/scala/extractors/FieldValidators.scala
@@ -9,19 +9,19 @@ import scala.collection.JavaConversions._
   * Created by andreas on 2017/02/15.
   */
 
-trait CDefExtractor[O <: CDef] extends Function1[Option[String], Option[O]] {
+class CDefExtractor[O <: CDef](cdefObj: O) extends Function1[Option[String], Option[O]] {
   override def apply(in: Option[String]): Option[O] = in
     .flatMap(inStr =>{
-      val byCode: Option[O] = ???
-      val byName: Option[O] = ???
-      val byAlias: Option[O] = ???
+      val byCode = Option(cdefObj.codeOf(inStr))
+      val byName = cdefObj.listAll().find(_.name().toLowerCase.replaceAll(" ", "") == inStr.toLowerCase.replaceAll(" ", ""))
+      val byAlias = cdefObj.listAll().find(_.alias().toLowerCase.replaceAll(" ", "") == inStr.toLowerCase.replaceAll(" ", ""))
       byCode orElse byName orElse byAlias
     })
 }
 
-object FlgExtractor extends CDefExtractor[Flg]
+object FlgExtractor extends CDefExtractor(Flg)
 
-object GenderExtractor extends CDefExtractor[Gender]
+object GenderExtractor extends CDefExtractor(Gender)
 
 //...
 
